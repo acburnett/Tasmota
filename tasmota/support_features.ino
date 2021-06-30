0f442f885d75deee2622a86960eabaadbf8762e5
acburnett <acburnett@gmail.com>
2021-06-30 01:43:04 -0400

    Update support_features.ino

--- tasmota/support_features.ino
+++ tasmota/support_features.ino
@@ -746,7 +746,9 @@ void ResponseAppendFeatures(void)
 #if defined(USE_I2C) && defined(USE_AM2320)
     feature8 |= 0x00000040;
 #endif
-//    feature8 |= 0x00000080;
+#ifdef USE_XYE_CCM
+    feature8 |= 0x00000080;  // xsns_89_xye_ccm.ino
+#endif
 
 //    feature8 |= 0x00000100;
 //    feature8 |= 0x00000200;
